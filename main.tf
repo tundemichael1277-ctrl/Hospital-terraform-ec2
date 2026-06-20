97279be580f6fcb147fea9cfa1339b99492d8358
babs <your_email@example.com>
2026-06-20 05:19:41 +0100

    new update

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "hospital_app" {
+    count = 4
+    ami = "ami-0150189e4c09ffab5"
+    instance_type = "t3.micro"
+    key_name = "mine"
+    security_groups = [ aws_security_group.hos_sg.name ]
+    tags = {
+      Name = "hospital_app"
+    }
+ 
+}
